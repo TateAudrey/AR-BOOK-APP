6976f0f71ef632eba85be4c860d7814362f3bc70
Audrey Chanakira <audrey@green.com.na>
2022-11-25 09:58:43 +0200

    Initial Commit: Basic Project Setup

---
+++ AR-BOOK-APP/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AR-BOOK-APP
+//
+//  Created by Audrey Chanakira on 11/25/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
