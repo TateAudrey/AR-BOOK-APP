55172ffc18e63822dbbaf8d2d120aee26b7371a7
Audrey Chanakira <audrey@green.com.na>
2022-11-25 15:32:37 +0200

    Initializing iOS Project

---
+++ AR-BOOK-APP/AppDelegate/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  AR-BOOK-APP
+//
+//  Created by Audrey Chanakira on 11/25/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+      
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+
+
+}
+
